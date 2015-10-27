e291958b844477b0b87fe2a0b1466b84a02cc220
Benjamin Haettasch <benjamin.haettasch@googlemail.com>
2015-10-27 14:01:44 +0100

    News stubs
    Not working correctly, just returning sample data to allow checks of the
    interface.

---
+++ news.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * AntenneBergstrasse API Adapater
+ * News: Get news overview
+ *
+ * Usage: news.php?page=1 (optional)
+ *
+ * @author Benjamin Haettasch (bhaettasch)
+ * @version 0.1a (stub)
+ */
+
+$news_entry = Array(
+    "id" => 123,
+    "headline" => "Headline",
+    "text" => "Text",
+    "teaser" => "Teaser",
+    "datum" => "1970-01-01 00:00:00",
+    "images" => Array(
+        Array("id"=>111, "url"=>"")
+    )
+);
+
+echo json_encode(Array($news_entry));
+
+?>
+
---
+++ news_detail.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * AntenneBergstrasse API Adapater
+ * News Detail: Get a single news entry based on a given id
+ *
+ * Usage: news_detail.php?id=123
+ *
+ * @author Benjamin Haettasch (bhaettasch)
+ * @version 0.1a (stub)
+ */
+
+$news_entry = Array(
+    "id" => 123,
+    "headline" => "Headline",
+    "text" => "Text",
+    "teaser" => "Teaser",
+    "datum" => "1970-01-01 00:00:00",
+    "images" => Array(
+            Array("id"=>111, "url"=>"")
+        )
+    );
+
+echo json_encode($news_entry);
+
+?>
