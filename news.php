81a4d18bc3019310829ca5cd1de932abe82ccadc
Benjamin Haettasch <benjamin.haettasch@googlemail.com>
2015-11-07 19:03:21 +0100

    News now using cache database

--- news.php
+++ news.php
@@ -6,23 +6,52 @@
  * Usage: news.php?page=1 (optional)
  *
  * @author Benjamin Haettasch (bhaettasch)
- * @version 0.1a (stub)
+ * @version 0.2
  */
+
+/*
+ * Setting: How many news items to return at once
+ */
+define("NEWS_PER_PAGE", 8);
 
 header('Content-Type: application/json;charset=UTF-8');
 
-$news_entry = Array(
-    "id" => 123,
-    "headline" => "Headline",
-    "text" => "Text",
-    "teaser" => "Teaser",
-    "datum" => "1970-01-01 00:00:00",
-    "images" => Array(
-        Array("id"=>111, "url"=>"")
-    )
-);
+require_once("db.php");
 
-echo json_encode(Array($news_entry));
+//Get id of page to load - default is 0, while the app starts with 1, thus this value has to be decremented by one
+$page = isset($_GET["page"]) ? (intval($_GET["page"], 10)-1) : 0;
+$start =  $page * NEWS_PER_PAGE;
+$end = $start + NEWS_PER_PAGE;
+
+$conn = db_connect("app");
+$res = $conn->query(sprintf("SELECT * FROM news_cache  ORDER BY date DESC LIMIT %d,%d;", $start, $end));
+
+if(!$res)
+{
+    echo "[]";
+}
+else
+{
+    $news = Array();
+
+    //Loop over all results:
+    while($row = $res->fetch_assoc())
+    {
+        $news[] = Array(
+            "id" => $row["id"],
+            "headline" => utf8_encode($row["headline"]),
+            "teaser" => utf8_encode($row["teaser"]),
+            "text" => utf8_encode($row["text"]),
+            "datum" => $row["date"],
+            "images" => Array(
+                Array("id"=>$row["id"], "url"=>$row["image_url"])
+            )
+        );
+    }
+
+    //Return array of news encoded as json
+    echo json_encode($news);
+}
 
 ?>
 
--- news_detail.php
+++ news_detail.php
@@ -6,22 +6,44 @@
  * Usage: news_detail.php?id=123
  *
  * @author Benjamin Haettasch (bhaettasch)
- * @version 0.1a (stub)
+ * @version 0.2
  */
 
 header('Content-Type: application/json;charset=UTF-8');
 
-$news_entry = Array(
-    "id" => 123,
-    "headline" => "Headline",
-    "text" => "Text",
-    "teaser" => "Teaser",
-    "datum" => "1970-01-01 00:00:00",
-    "images" => Array(
-            Array("id"=>111, "url"=>"")
-        )
-    );
+if(!isset($_GET["id"]) || $_GET["id"]=="")
+{
+    echo "[]";
+}
+else
+{
+    require_once("db.php");
 
-echo json_encode($news_entry);
+    $conn = db_connect("app");
+
+    //Get news article from cache
+    $res = $conn->query(sprintf("SELECT * FROM `news_cache` WHERE `id`=%d", $_GET["id"]));
+
+    if(!$res || !($row = $res->fetch_assoc()))
+    {
+        http_response_code(404);
+        echo '{"message":"News article with this id not found"}';
+    }
+    else
+    {
+        http_response_code(200);
+        $news_entry = Array(
+            "id" => $row["id"],
+            "headline" => utf8_encode($row["headline"]),
+            "teaser" => utf8_encode($row["teaser"]),
+            "text" => utf8_encode($row["text"]),
+            "datum" => $row["date"],
+            "images" => Array(
+                Array("id"=>$row["id"], "url"=>$row["image_url"])
+            )
+        );
+        echo json_encode($news_entry);
+    }
+}
 
 ?>
