1bc5511d19381c1258b4ce8b4322280cd76e3afe
Benjamin Haettasch <benjamin.haettasch@googlemail.com>
2015-11-07 14:24:40 +0100

    Improved content of schedule

--- schedule.php
+++ schedule.php
@@ -48,7 +48,7 @@ else
     $date_id = $row['id'];
     $date = new DateTime(sprintf("%d-%d-%d 00:00:00", $date_array[0], $date_array[1], $date_array[2]));
 
-    $res = $conn->query(sprintf("SELECT `show`.`id`, `rail`.`name` AS railname, `show`.`name` AS showname, `rail`.`description` AS raildescription, `show`.`description` AS showdescription, time_start, time_end FROM `show`, `rail` WHERE `date_id` = %d AND `rail`.`id` = rail_id ORDER BY `rail`.`time_start` ASC", $date_id));
+    $res = $conn->query(sprintf("SELECT `show`.`id`, `rail`.`name` AS railname, `show`.`name` AS showname, `rail`.`description` AS raildescription, `show`.`description` AS `showdescription`, `time_start`, `time_end`, `e1`.`firstname` AS `e1firstname`, `e2`.`firstname` AS `e2firstname`, `e3`.`firstname` AS `e3firstname`, `e1`.`lastname` AS `e1lastname`, `e2`.`lastname` AS `e2lastname`, `e3`.`lastname` AS `e3lastname` FROM `show` LEFT JOIN `rail` ON `show`.`rail_id` = `rail`.`id` LEFT JOIN `show_team` ON `show`.`id` = `show_team`.`show_id` LEFT JOIN `team` AS `e1` ON `show_team`.`editor1_id`=`e1`.`id` LEFT JOIN `team` AS `e2` ON `show_team`.`editor2_id`=`e2`.`id` LEFT JOIN `team` AS `e3` ON `show_team`.`editor3_id`=`e3`.`id` WHERE `date_id` = %d ORDER BY `rail`.`time_start` ", $date_id));
 
     if(!$res)
     {
@@ -60,14 +60,26 @@ else
     //Loop over all results:
     while($row = $res->fetch_assoc())
     {
+        //Create detail information for this entry
+        $detail = "";
+        if($row["showname"] != "")
+            $detail = "<b>".$row["showname"]."</b><br><br>";
+        $detail .= $row["showdescription"] != "" ? $row["showdescription"] : $row["raildescription"];
+
+        if($row["e3lastname"] !="")
+            $detail .= sprintf("<br><br>Mit %s %s, %s %s und %s %s", $row["e1firstname"], $row["e1lastname"], $row["e2firstname"], $row["e2lastname"], $row["e3firstname"], $row["e3lastname"]);
+        elseif($row["e2lastname"] !="")
+            $detail .= sprintf("<br><br>Mit %s %s und %s %s", $row["e1firstname"], $row["e1lastname"], $row["e2firstname"], $row["e2lastname"]);
+        elseif($row["e1lastname"] !="")
+            $detail .= sprintf("<br><br>Mit %s %s", $row["e1firstname"], $row["e1lastname"]);
+
         //Create an entry for every result...
         $schedule_item = Array(
             "id" => $row["id"],
-            "subject" => utf8_encode($row["showname"]),
-            "description" => utf8_encode($row["showdescription"]),
+            "subject" => utf8_encode($detail),
             "start" => convertTimeToTimestamp($row["time_start"], $date),
             "end" => convertTimeToTimestamp($row["time_end"], $date),
-            "redaktionen" => Array("name"=> utf8_encode(html_entity_decode($row["railname"])), "description"=>utf8_encode($row["raildescription"]))
+            "redaktionen" => Array("name"=> utf8_encode(html_entity_decode($row["railname"])))
         );
 
         //...store it...
