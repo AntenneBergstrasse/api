e6d10a0d001925e0300720c0dd1c3dc13ee67b29
Benjamin Haettasch <benjamin.haettasch@googlemail.com>
2015-11-09 12:02:17 +0100

    Further improved song query

--- song.php
+++ song.php
@@ -14,7 +14,7 @@ require_once("db.php");
 $conn = db_connect("music");
 
 // Get current song from database
-$res = $conn->query("SELECT artist, title FROM logging WHERE ((duration > 60 OR active = 1) AND (DATE_SUB(CURDATE(),INTERVAL 15 MINUTE ) <= datetime)) AND (title != '' AND artist != '') ORDER BY datetime DESC LIMIT 0,1;");
+$res = $conn->query("SELECT artist, title FROM logging WHERE active = 1 AND (DATE_SUB(CURDATE(),INTERVAL 15 MINUTE ) <= datetime) AND (title != '' AND artist != '') ORDER BY datetime DESC LIMIT 0,1;");
 
 if(!$res || !($row = $res->fetch_assoc()))
 {
