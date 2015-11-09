3eecd4b647e1efcc6f5e503b94eb84064ac26279
Benjamin Haettasch <benjamin.haettasch@googlemail.com>
2015-11-09 02:21:57 +0100

    Improved song
    Better selection criterias in query
    Added default cover

--- song.php
+++ song.php
@@ -14,16 +14,15 @@ require_once("db.php");
 $conn = db_connect("music");
 
 // Get current song from database
-$res = $conn->query("SELECT artist, title FROM logging WHERE ((active = 1 AND DATE_SUB(CURDATE(),INTERVAL 15 MINUTE ) <= datetime)  OR duration > 60) AND (title != '' AND artist != '') ORDER BY datetime DESC LIMIT 0,1;");
+$res = $conn->query("SELECT artist, title FROM logging WHERE ((duration > 60 OR active = 1) AND (DATE_SUB(CURDATE(),INTERVAL 15 MINUTE ) <= datetime)) AND (title != '' AND artist != '') ORDER BY datetime DESC LIMIT 0,1;");
 
-if(!$res)
+if(!$res || !($row = $res->fetch_assoc()))
 {
     echo "[]";
 }
 else
 {
-    $row = $res->fetch_assoc();
-    $song = Array("id"=>0, "title"=>utf8_encode($row['title']), "artist"=>utf8_encode($row['artist']), "cover"=>"");
+    $song = Array("id"=>0, "title"=>utf8_encode($row['title']), "artist"=>utf8_encode($row['artist']), "cover"=>SONG_DEFAULT_COVER);
     echo json_encode($song);
 }
 $conn->close();
